2d1c8d23656f59482d023554d844b980f5dd39a3
chaboiyungsteve <55422540+chaboiyungsteve@users.noreply.github.com>
2020-02-07 10:57:10 -0700

    Add files via upload

---
+++ butt_test.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+#set up GPIO using BCM numbering
+GPIO.setmode(GPIO.BCM)
+
+#setup GPIO using Board numbering
+#GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(4, GPIO.IN, buttSignal = GPIO.PUD_DOWN)
+
+while True:
+    if(GPIO.input(4) ==1):
+        print(“Button 1 pressed”)
