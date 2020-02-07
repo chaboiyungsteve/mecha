ac174bc1cbd8655170c4d53f22ff941282a332e3
chaboiyungsteve <55422540+chaboiyungsteve@users.noreply.github.com>
2020-02-07 11:19:55 -0700

    Update butt_test.py

--- butt_test.py
+++ butt_test.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 #set up GPIO using BCM numbering
 GPIO.setmode(GPIO.BCM)
@@ -12,3 +13,4 @@ GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 while True:
     if (GPIO.input(4) == 1):
         print("dd")
+        time.sleep(3)
