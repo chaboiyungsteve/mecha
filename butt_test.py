210dc5762cd03518e52cd1f532775bd1431b9459
Stephen Evans <Esteban@engr2-20-72-dhcp.int.colorado.edu>
2020-02-10 12:27:58 -0700

    v

--- butt_test.py
+++ butt_test.py
@@ -32,7 +32,7 @@ while (game == True and button == 0):
     i = i+1
     if i == 25:
         i = 18
-    if button == 0:
+    if button == 1:
         break
 
         # while (game == True and GPIO.input(4) == 0):
