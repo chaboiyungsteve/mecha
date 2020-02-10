4cd9a7bbc3207f29a83aef26edbeb675aa8be035
Stephen Evans <Esteban@engr2-20-72-dhcp.int.colorado.edu>
2020-02-10 10:20:25 -0700

    LED test

--- butt_test.py
+++ butt_test.py
@@ -13,9 +13,25 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # LED Pins Setup
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+
+ledClock = .1
 
 while True:
-    if (GPIO.input(4) == 1):
-        print("dd")
-        time.sleep(.1)
-        GPIO.output(18, GPIO.HIGH)
+    for i in range(18, 25):
+        GPIO.output(i, GPIO.HIGH)
+        time.sleep(ledClock)
+        GPIO.output(i, GPIO.LOW)
+        time.sleep(ledClock)
+
+
+# while True:
+#     if (GPIO.input(4) == 1):
+#         print("dd")
+#         time.sleep(.1)
+#         GPIO.output(18, GPIO.HIGH)
