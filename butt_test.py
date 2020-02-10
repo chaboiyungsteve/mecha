0e56a2903e6e5507b3999599806de8cfa114042b
Stephen Evans <Esteban@engr2-20-72-dhcp.int.colorado.edu>
2020-02-10 10:05:53 -0700

    test

--- butt_test.py
+++ butt_test.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-#set up GPIO using BCM numbering
+# this is a test comment
+
+# set up GPIO using BCM numbering
 GPIO.setmode(GPIO.BCM)
 
 
-#setup GPIO using Board numbering
-#GPIO.setmode(GPIO.BOARD)
+# setup GPIO using Board numbering
+# GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     if (GPIO.input(4) == 1):
