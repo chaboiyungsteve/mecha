b0326c72a5f6931f899f75fdad321dd6b29b1160
Stephen Evans <Esteban@engr2-20-72-dhcp.int.colorado.edu>
2020-02-10 10:32:27 -0700

    ghj

--- butt_test.py
+++ butt_test.py
@@ -4,10 +4,6 @@ import time
 # set up GPIO using BCM numbering
 GPIO.setmode(GPIO.BCM)
 
-
-# setup GPIO using Board numbering
-# GPIO.setmode(GPIO.BOARD)
-
 # Button Pin Setup
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -29,6 +25,7 @@ while True:
         GPIO.output(i, GPIO.LOW)
         time.sleep(ledClock)
     if (GPIO.input(4) == 1):
-        break
         GPIO.output(i, GPIO.HIGH)
-    GPIO.cleanup()
+        break
+
+GPIO.cleanup()
