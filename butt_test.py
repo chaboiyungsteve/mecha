172e27f07e709d32721cbcb5c78002b7de10fee6
Stephen Evans <Esteban@engr2-20-72-dhcp.int.colorado.edu>
2020-02-10 10:12:52 -0700

    LED test

--- butt_test.py
+++ butt_test.py
@@ -1,8 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-# this is a test comment
-
 # set up GPIO using BCM numbering
 GPIO.setmode(GPIO.BCM)
 
@@ -10,9 +8,14 @@ GPIO.setmode(GPIO.BCM)
 # setup GPIO using Board numbering
 # GPIO.setmode(GPIO.BOARD)
 
+# Button Pin Setup
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# LED Pins Setup
+GPIO.setup(18, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     if (GPIO.input(4) == 1):
         print("dd")
         time.sleep(.1)
+        GPIO.output(18, GPIO.HIGH)
