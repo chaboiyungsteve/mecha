39c65110e7773fd96bcd3f36a5c0456cce164f89
Evans <stev2368@itll.cu.int>
2020-02-21 10:36:41 -0700

    test

--- auto_cyclone.py
+++ auto_cyclone.py
@@ -6,9 +6,11 @@ cap = cv2.VideoCapture(0)
 while(1):
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    thresh  = 40
+    targColor = [0,0,0]
     
-    lower_red = np.array([30,150,50])
-    upper_red = np.array([255,255,180])
+    lower_red = np.array(targColor-thresh)
+    upper_red = np.array(targColor+thresh)
     
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame,frame, mask= mask)
