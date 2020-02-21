8c4496a0bed13643545ac006756b6f90f50a4e62
Evans <stev2368@itll.cu.int>
2020-02-21 10:48:47 -0700

    test

--- auto_cyclone.py
+++ auto_cyclone.py
@@ -3,25 +3,25 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-while(1):
+while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    thresh  = [40,40,40]
-    targColor = [40,40,40]
-    
-    lower_red = np.array(targColor-thresh)
-    upper_red = np.array(targColor+thresh)
-    
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    thresh = 40
+    targColor = [40, 217, 200]
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    
-    k = cv2.waitKey(5) & 0xFF
+    lower_green = np.array(targColor-thresh)
+    upper_green = np.array(targColor+thresh)
+
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+
+    k = cv2.waitKey(5) & 0xff
     if k == 27:
         break
 
-cv2.destroyAllWindows()
+cv2.destryAllWindows()
 cap.release()
