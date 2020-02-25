261948e8248e6df157395ef662a69d831af22b4e
stevenephans <stephen.w.evans@gmail.com>
2020-02-25 13:05:40 -0700

    g

---
+++ onlyRed.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Range for lower red
+    lower_red = np.array([0,120,70])
+    upper_red = np.array([10,255,255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+ 
+    # Range for upper range
+    lower_red = np.array([170,120,70])
+    upper_red = np.array([180,255,255])
+    mask2 = cv2.inRange(hsv,lower_red,upper_red)
+    mask=mask1+mask2
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+
+    k = cv2.waitKey(5) & 0xff
+    if k == 27:
+        break
+
+cv2.destryAllWindows()
+cap.release()
